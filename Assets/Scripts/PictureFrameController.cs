60829c8731ca9a5c60ed89a21f29e9431fae7df9
Niklas Tluk <niklas.tluk@study.hs-duesseldorf.de>
2022-12-17 18:44:26 +0100

    rewrote picture swap mechanic based on eye tracking API

--- Assets/Scripts/PictureFrameController.cs
+++ Assets/Scripts/PictureFrameController.cs
@@ -2,13 +2,22 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Tobii.G2OM;
+using Tobii.XR;
 
 public class PictureFrameController : MonoBehaviour
 {
     public Camera userCamera;
     public List<Material> picturePool = new List<Material>();
     public List<Material> pictureSelection = new List<Material>();
-    public GameObject[] pictureFrames = new GameObject[2]; //6
+    // public GameObject[] pictureFrames = new GameObject[2]; //6
+    public GameObject pictureFL;
+    public GameObject pictureFR;
+    public GameObject pictureL;
+    public GameObject pictureR;
+    public GameObject pictureBL;
+    public GameObject pictureBR;
+    private GameObject focused;
     
     System.Random rnd;
 
@@ -21,21 +30,61 @@ public class PictureFrameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        foreach (GameObject frame in pictureFrames)
+    {   // Check whether TobiiXR has any focused objects.
+        if (TobiiXR.FocusedObjects.Count > 0)
         {
-            Debug.Log(Math.Abs(Math.Abs(userCamera.transform.eulerAngles.y)));
-            if (Math.Abs(Math.Abs(userCamera.transform.eulerAngles.y) - Math.Abs(frame.transform.eulerAngles.y)) >= 175 && Math.Abs(userCamera.transform.eulerAngles.y) <= 275)
+            focused = TobiiXR.FocusedObjects[0].GameObject;
+            int pos = rnd.Next(0, pictureSelection.Count);
+           
+            switch (focused.name)
             {
-                int pos = rnd.Next(0, pictureSelection.Count);
-                if (pictureSelection[pos] != null && pictureSelection.Count > 0)
-                {
-                    frame.GetComponent<Renderer>().material = pictureSelection[pos];
-                    // pictureSelection.Remove(pictureSelection[pos]);
-                    frame.GetComponentInChildren<TextMeshPro>().text = frame.GetComponent<Renderer>().sharedMaterial.name;
-                }
+                case "PictureFL":
+                    // add cooldown if clause
+                    pictureBL.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
+                case "PictureFR":
+                    // add cooldown if clause
+                    pictureBR.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
+                case "PictureL":
+                    // add cooldown if clause
+                    pictureR.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
+                case "PictureR":
+                    // add cooldown if clause
+                    pictureL.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
+                case "PictureBL":
+                    // add cooldown if clause
+                    pictureFL.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
+                case "PictureBR":
+                    // add cooldown if clause
+                    pictureFR.GetComponent<Renderer>().material = pictureSelection[pos];
+                    break;
+
             }
         }
+       
+        //foreach (GameObject frame in pictureFrames)
+        //{
+        //    Debug.Log(Math.Abs(Math.Abs(userCamera.transform.eulerAngles.y)));
+        //    if (Math.Abs(Math.Abs(userCamera.transform.eulerAngles.y) - Math.Abs(frame.transform.eulerAngles.y)) >= 175 && Math.Abs(userCamera.transform.eulerAngles.y) <= 275)
+        //    {
+        //        int pos = rnd.Next(0, pictureSelection.Count);
+        //        if (pictureSelection[pos] != null && pictureSelection.Count > 0)
+        //        {
+        //            frame.GetComponent<Renderer>().material = pictureSelection[pos];
+        //            // pictureSelection.Remove(pictureSelection[pos]);
+        //            frame.GetComponentInChildren<TextMeshPro>().text = frame.GetComponent<Renderer>().sharedMaterial.name;
+        //        }
+        //    }
+        //}
     }
 
     private void SelectFromPool()
